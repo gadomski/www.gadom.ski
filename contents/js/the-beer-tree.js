27617b21ee2ef745fc830bbf263c47379cd37985
Pete Gadomski <pete.gadomski@gmail.com>
2014-07-03 10:36:35 -0600

    Convert padding to margin

--- contents/js/the-beer-tree.js
+++ contents/js/the-beer-tree.js
@@ -2,7 +2,8 @@ var kegLife = d3.select("#keg-life"),
     calendar = d3.select("#calendar"),
     costComparison = d3.select("#costComparison");
 
-var width = Number(kegLife.style("width").slice(0, kegLife.style("width").length - 2));
+var width = Number(kegLife.style("width").slice(0, kegLife.style("width").length - 2)),
+    margin = {top: 40, right: 10, bottom: 0, left: 100};
 
 var parseDate = d3.time.format("%Y-%m-%d");
 
@@ -11,54 +12,32 @@ function msToDays(d) { return Math.floor(d * 1.15741e-8); }
 
 
 function buildKegLife(data) {
-    var height = width / 2
-        padding = {top: 40, right: 10, bottom: 0, left: 100};
+    var height = width / 2;
 
-    var kegPurchases = data["purchases"].filter(function(d) {
-        return d.keg;
-    });
-    var maxDateRange;
-    var today = new Date();
-    today.setHours(0, 0, 0, 0);
-
-    kegPurchases.forEach(function(d, i, a) {
-        if (a[i + 1])
-        {
-            d.endDate = a[i + 1].date;
-            d.kicked = true;
-        }
-        else
-        {
-            d.endDate = today;
-            d.kicked = false;
-        }
-        d.dateRange = d.endDate - d.date;
-        if (!maxDateRange || d.dateRange > maxDateRange)
-            maxDateRange = d.dateRange;
-    });
+    var maxDateRange = d3.max(data.kegPurchases, function(d) { return d.dateRange; });
 
     var xscale = d3.scale.linear()
         .domain([0, maxDateRange])
-        .range([0, width - padding.right - padding.left]);
+        .range([0, width]);
 
     var xaxis = d3.svg.axis()
         .orient("top")
         .scale(xscale)
         .tickFormat(function(d) { return msToDays(d) + " days"; });
 
-    var barHeight = (height - padding.top - padding.bottom) / kegPurchases.length;
+    var barHeight = (height - margin.top - margin.bottom) / data.kegPurchases.length;
     var barSpacing = 10;
 
     var svg = kegLife.append("svg")
-        .attr("width", width)
-        .attr("height", height);
+        .attr("width", width + margin.right + margin.left)
+        .attr("height", height + margin.top + margin.bottom);
 
-    var bar = svg.selectAll("g.bar").data(kegPurchases);
+    var bar = svg.selectAll("g.bar").data(data.kegPurchases);
     bar.enter().append("g")
         .attr("class", "bar");
     bar
         .attr("transform", function(d, i) {
-            return "translate(" + padding.left + "," + (i * barHeight + padding.top + barSpacing) + ")";
+            return "translate(" + margin.left + "," + (i * barHeight + margin.top + barSpacing) + ")";
         })
     bar.append("rect")
         .attr("width", function(d) { return xscale(d.dateRange); })
@@ -86,14 +65,39 @@ function buildKegLife(data) {
 
     svg.append("g")
         .attr("class", "x axis")
-        .attr("transform", "translate(" + padding.left + "," + padding.top + ")")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
         .call(xaxis);
 }
 
 
+function buildCostComparison(data) {
+    var height = width / 2;
+
+}
+
+
 d3.json("../data/the-beer-tree.json", function(error, data) {
-    data["purchases"].forEach(function(d) {
+    var today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    data.kegPurchases = data["purchases"].filter(function(d) {
         d.date = parseDate.parse(d.date);
+        return d.keg;
     });
+    data.kegPurchases.forEach(function(d, i, a) {
+        if (a[i + 1])
+        {
+            d.endDate = a[i + 1].date;
+            d.kicked = true;
+        }
+        else
+        {
+            d.endDate = today;
+            d.kicked = false;
+        }
+        d.dateRange = d.endDate - d.date;
+    });
+
     buildKegLife(data);
+    buildCostComparison(data);
 });
